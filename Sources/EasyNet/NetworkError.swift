704935fb5ed9e2ce5c86bacbe9779c9deb3b0cbb
Md. Rezaul Islam Tarek <33925245+rezaulislamtarek@users.noreply.github.com>
2024-12-04 17:59:10 +0600

    Update NetworkError.swift

--- Sources/EasyNet/NetworkError.swift
+++ Sources/EasyNet/NetworkError.swift
@@ -1,18 +1,18 @@
 //
-//  File.swift
-//  
+//  EasyNetError.swift
 //
-//  Created by Rezaul Islam on 30/6/24.
+//  Created by Rezaul Islam on 4/12/24.
 //
 
 import Foundation
 
-enum NetworkError : Error{
+public enum EasyNetError : Error{
     case invalidURL
     case badServerResponse
     case responseError
     case unknown
     case unauthorized
     case bodyPerseError
+    case decodeError(Error)
     case validationError(Data)
 }
