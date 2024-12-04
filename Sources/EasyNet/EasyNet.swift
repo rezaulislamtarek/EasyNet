2e152915f485f48d577294ca0c0325a7e5d21738
Md. Rezaul Islam Tarek <33925245+rezaulislamtarek@users.noreply.github.com>
2024-12-04 17:57:55 +0600

    Update EasyNet.swift

--- Sources/EasyNet/EasyNet.swift
+++ Sources/EasyNet/EasyNet.swift
@@ -1,7 +1,13 @@
+//
+//  EasyNetError.swift
+//
+//  Created by Rezaul Islam on 4/12/24.
+//
+
 import Foundation
 import AVFoundation
 import UIKit
-import Combine 
+import Combine
 
 
 public class EasyNet{
@@ -18,47 +24,19 @@ public class EasyNet{
     
     
     
+    
     public func fetchData<T : Codable>(endPoint : String, responseType : T.Type, extraHeaders : [String : String]? = nil) -> AnyPublisher<T, Error>{
-        var request = URLRequest(url: URL(string: baseUrl+endPoint)!)
-        request.httpMethod = HTTPMethods.get.rawValue
-        request.allHTTPHeaderFields = getHeaders(extraHeaders)
-        
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        
-        let publisher =
-        URLSession.shared.dataTaskPublisher(for: request)
-            .tryMap { (data: Data, response : URLResponse) in
-                guard let httpResponse = response as? HTTPURLResponse else{
-                    throw NetworkError.badServerResponse
-                }
-                if httpResponse.statusCode == 401 {
-                    DispatchQueue.main.async {
-                        self.unauthorizedSubject.send(())
-                    }
-                    throw NetworkError.unauthorized
-                }
-                guard httpResponse.statusCode == 200 else {
-                    throw NetworkError.badServerResponse
-                }
-                print(data)
-                return data
-            }
-            .decode(type: responseType.self, decoder: decoder)
-            .retry(0)
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
-        
-        return publisher
+        guard var request = createRequest(withEndPoint: endPoint, httpMethod: .get) else {
+            return Fail(error: EasyNetError.invalidURL).eraseToAnyPublisher()
+        }
+        return handelRequest(request: &request, extraHeaders: extraHeaders, responseType: responseType)
     }
     
     public func postData<T: Codable, R: Codable>(endPoint: String, requestBody: R? = nil, responseType: T.Type, extraHeaders: [String: String]? = nil) -> AnyPublisher<T, Error> {
-        var request = URLRequest(url: URL(string: baseUrl + endPoint)!)
-        request.httpMethod = HTTPMethods.post.rawValue
-        request.allHTTPHeaderFields = getHeaders(extraHeaders)
         
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        guard var request = createRequest(withEndPoint: endPoint, httpMethod: .post) else {
+            return Fail(error: EasyNetError.invalidURL).eraseToAnyPublisher()
+        }
         
         // Prepare request body
         do {
@@ -66,40 +44,19 @@ public class EasyNet{
             encoder.keyEncodingStrategy = .convertToSnakeCase
             request.httpBody = try encoder.encode(requestBody)
         } catch {
-            return Fail(error: NetworkError.bodyPerseError)
+            return Fail(error: EasyNetError.bodyPerseError)
                 .eraseToAnyPublisher()
         }
         
-        return URLSession.shared.dataTaskPublisher(for: request)
-            .tryMap { (data: Data, response: URLResponse) -> Data in
-                guard let httpResponse = response as? HTTPURLResponse else {
-                    throw NetworkError.badServerResponse
-                }
-                if (200..<300).contains(httpResponse.statusCode) {
-                    return data
-                } else if httpResponse.statusCode == 422 {
-                    throw NetworkError.validationError(data)
-                } else if httpResponse.statusCode == 400 {
-                    throw NetworkError.bodyPerseError
-                } else {
-                    throw NetworkError.unknown
-                }
-            }
-            .mapError { $0 as Error }  // Convert any error to Error type
-            .flatMap { data -> AnyPublisher<T, Error> in
-                Just(data)
-                    .decode(type: responseType, decoder: decoder)
-                    .mapError { $0 as Error }  // Convert decoding errors to Error type
-                    .eraseToAnyPublisher()
-            }
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
+        return handelRequest(request: &request, extraHeaders: extraHeaders, responseType: responseType)
+        
     }
     
     public func putData<T: Codable, R: Codable>(endPoint: String, requestBody: R? = nil, responseType: T.Type, extraHeaders: [String: String]? = nil) -> AnyPublisher<T, Error> {
-        var request = URLRequest(url: URL(string: baseUrl + endPoint)!)
-        request.httpMethod = HTTPMethods.put.rawValue
-        request.allHTTPHeaderFields = getHeaders(extraHeaders)
+        
+        guard var request = createRequest(withEndPoint: endPoint, httpMethod: .put) else {
+            return Fail(error: EasyNetError.invalidURL).eraseToAnyPublisher()
+        }
         
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
@@ -110,60 +67,43 @@ public class EasyNet{
             encoder.keyEncodingStrategy = .convertToSnakeCase
             request.httpBody = try encoder.encode(requestBody)
         } catch {
-            return Fail(error: NetworkError.bodyPerseError)
+            return Fail(error: EasyNetError.bodyPerseError)
                 .eraseToAnyPublisher()
         }
-        
-        let publisher =
-        URLSession.shared.dataTaskPublisher(for: request)
-            .tryMap { (data: Data, response: URLResponse) in
-                guard let httpResponse = response as? HTTPURLResponse else {
-                    throw NetworkError.badServerResponse
-                }
-                if (200..<300).contains(httpResponse.statusCode) {
-                    return data
-                } else {
-                    if httpResponse.statusCode == 422 {
-                        throw NetworkError.validationError(data)
-                    } else {
-                        throw NetworkError.unknown
-                    }
-                }
-            }
-            .decode(type: responseType, decoder: decoder)
-            .retry(0)
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
-        
-        return publisher
+        return handelRequest(request: &request, extraHeaders: extraHeaders, responseType: responseType)
     }
     
-    
-    func uploadMultipartFormData<T: Codable>(endPoint: String, responseType: T.Type, image: UIImage, keyForImage: String) -> AnyPublisher<T, Error> {
-        
-        guard let url = URL(string: baseUrl + endPoint) else {
-            return Fail(error: NetworkError.invalidURL)
-                .eraseToAnyPublisher()
+    public func deleteData<T: Codable>(endPoint: String, responseType: T.Type, extraHeaders: [String: String]? = nil) -> AnyPublisher<T, Error> {
+        guard var request = createRequest(withEndPoint: endPoint, httpMethod: .delete) else {
+            return Fail(error: EasyNetError.invalidURL).eraseToAnyPublisher()
         }
         
-        var formDataFields = [(String, String)]()
+        return handelRequest(request: &request, extraHeaders: extraHeaders, responseType: responseType)
+    }
+    
+    public func uploadMultipartFormData<T: Codable>(endPoint: String, responseType: T.Type, image: UIImage, keyForImage: String) -> AnyPublisher<T, Error> {
+        
+        guard var request = createRequest(withEndPoint: endPoint, httpMethod: .post) else {
+            return Fail(error: EasyNetError.invalidURL).eraseToAnyPublisher()
+        }
         
         guard let imageData = image.jpegData(compressionQuality: 0.8) else {
-            return Fail(error: NetworkError.bodyPerseError)
+            return Fail(error: EasyNetError.bodyPerseError)
                 .eraseToAnyPublisher()
         }
-        
         
         let boundary = UUID().uuidString
         let contentType = "multipart/form-data; boundary=\(boundary)"
         
-        var request = URLRequest(url: url)
-        request.httpMethod = HTTPMethods.post.rawValue
-        request.allHTTPHeaderFields = getHeaders()
         request.setValue(contentType, forHTTPHeaderField: "Content-Type")
+        request.httpBody = prepareMultipartBody(boundary: boundary, imageData: imageData, keyForImage: keyForImage)
         
+        return handelRequest(request: &request, responseType: responseType)
+    }
+    
+    
+    private func prepareMultipartBody(boundary : String, imageData : Data, keyForImage : String) -> Data{
         var body = Data()
-        
         // Generate a unique filename for the image
         let uniqueFilename = "\(UUID().uuidString).JPEG"
         
@@ -174,156 +114,10 @@ public class EasyNet{
         body.append(imageData)
         body.append("\r\n".data(using: .utf8)!)
         
-        
-        
         // Close the multipart form data body
         body.append("--\(boundary)--\r\n".data(using: .utf8)!)
-        
-        request.httpBody = body
-        
-         
-        
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        
-        
-        return URLSession.shared.dataTaskPublisher(for: request)
-            .tryMap { (data: Data, response: URLResponse) in
-                guard let httpResponse = response as? HTTPURLResponse else {
-                    throw NetworkError.responseError
-                }
-                if httpResponse.statusCode == 401 {
-                    throw NetworkError.unknown
-                }
-                return data
-            }
-            .decode(type: T.self, decoder: decoder)
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
+        return body
     }
-    
-    
-    
-    func compressVideo(inputURL: URL, outputURL: URL, completion: @escaping (URL?) -> Void) {
-        let asset = AVAsset(url: inputURL)
-        guard let exportSession = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetMediumQuality) else {
-            completion(nil)
-            return
-        }
-        
-        exportSession.outputURL = outputURL
-        exportSession.outputFileType = .mp4
-        exportSession.shouldOptimizeForNetworkUse = true
-        
-        exportSession.exportAsynchronously {
-            switch exportSession.status {
-            case .completed:
-                completion(outputURL)
-            default:
-                completion(nil)
-            }
-        }
-    }
-    
-    func uploadMultipartFormDataWithCompressedVideo<T: Codable>(
-        endPoint: String,
-        responseType: T.Type,
-        images: [UIImage],
-        keyForImages: String,
-        videoURL: URL,
-        keyForVideo: String,
-        textFields: [String: String]
-    ) -> AnyPublisher<T, Error> {
-        
-        // Create a temporary URL for the compressed video
-        let compressedVideoURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(UUID().uuidString).appendingPathExtension("mp4")
-        
-        // Compress the video
-        return Future<URL?, Never> { promise in
-            self.compressVideo(inputURL: videoURL, outputURL: compressedVideoURL) { compressedURL in
-                promise(.success(compressedURL))
-            }
-        }
-        .compactMap { $0 }
-        .flatMap { compressedURL -> AnyPublisher<T, Error> in
-            guard let url = URL(string: self.baseUrl + endPoint) else {
-                return Fail(error: NetworkError.invalidURL).eraseToAnyPublisher()
-            }
-            
-            let boundary = UUID().uuidString
-            let contentType = "multipart/form-data; boundary=\(boundary)"
-            
-            var request = URLRequest(url: url)
-            request.httpMethod = HTTPMethods.post.rawValue
-            request.allHTTPHeaderFields = self.getHeaders()
-            request.setValue(contentType, forHTTPHeaderField: "Content-Type")
-            
-            var body = Data()
-            
-            // Append images
-            for (index, image) in images.enumerated() {
-                guard let imageData = image.jpegData(compressionQuality: 0.5) else {
-                    return Fail(error: NetworkError.bodyPerseError).eraseToAnyPublisher()
-                }
-                
-                let uniqueFilename = "\(UUID().uuidString).jpg"
-                body.append("--\(boundary)\r\n".data(using: .utf8)!)
-                body.append("Content-Disposition: form-data; name=\"\(keyForImages)[\(index)]\"; filename=\"\(uniqueFilename)\"\r\n".data(using: .utf8)!)
-                body.append("Content-Type: image/jpeg\r\n\r\n".data(using: .utf8)!)
-                body.append(imageData)
-                body.append("\r\n".data(using: .utf8)!)
-            }
-            
-            // Append compressed video
-            if let videoData = try? Data(contentsOf: compressedURL) {
-                let uniqueFilename = "\(UUID().uuidString).mp4"
-                body.append("--\(boundary)\r\n".data(using: .utf8)!)
-                body.append("Content-Disposition: form-data; name=\"\(keyForVideo)\"; filename=\"\(uniqueFilename)\"\r\n".data(using: .utf8)!)
-                body.append("Content-Type: video/mp4\r\n\r\n".data(using: .utf8)!)
-                body.append(videoData)
-                body.append("\r\n".data(using: .utf8)!)
-            } else {
-                return Fail(error: NetworkError.bodyPerseError).eraseToAnyPublisher()
-            }
-            
-            // Append text fields
-            for (key, value) in textFields {
-                body.append("--\(boundary)\r\n".data(using: .utf8)!)
-                body.append("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n".data(using: .utf8)!)
-                body.append("\(value)\r\n".data(using: .utf8)!)
-            }
-            
-            // Close the multipart form data body
-            body.append("--\(boundary)--\r\n".data(using: .utf8)!)
-            
-            request.httpBody = body
-            
-            // Debug logging
-            print("Request URL: \(url)")
-            print("Request Headers: \(request.allHTTPHeaderFields ?? [:])")
-            print("Request Body Length: \(body.count) bytes")
-            
-            return URLSession.shared.dataTaskPublisher(for: request)
-                .tryMap { (data: Data, response: URLResponse) in
-                    if let httpResponse = response as? HTTPURLResponse {
-                        print("Response Status Code: \(httpResponse.statusCode)")
-                        if httpResponse.statusCode != 200 {
-                            let responseString = String(data: data, encoding: .utf8) ?? "Unknown response"
-                            print("Response: \(responseString)")
-                            throw NetworkError.responseError
-                        }
-                    } else {
-                        throw NetworkError.responseError
-                    }
-                    return data
-                }
-                .decode(type: T.self, decoder: JSONDecoder())
-                .receive(on: DispatchQueue.main)
-                .eraseToAnyPublisher()
-        }
-        .eraseToAnyPublisher()
-    }
-    
     
     private func getHeaders(_ extraHeaders : [String : String]? = nil) -> [String: String] {
         var headers: [String: String] = [:]
@@ -335,9 +129,55 @@ public class EasyNet{
         headers["Content-Type"] = "application/json"
         headers["Accept"] = "application/json"
         headers["Authorization"] = token
-       
-        
-        
         return headers
+    }
+    
+    private func handelRequest<T: Codable>( request : inout URLRequest, extraHeaders : [String : String]? = nil, responseType : T.Type) -> AnyPublisher<T, Error>{
+        request.allHTTPHeaderFields = getHeaders(extraHeaders)
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let publisher =
+        URLSession.shared.dataTaskPublisher(for: request)
+            .tryMap { [weak self] (data: Data, response: URLResponse) in
+                guard let self = self else { throw EasyNetError.badServerResponse }
+                guard let httpResponse = response as? HTTPURLResponse else{
+                    throw EasyNetError.badServerResponse
+                }
+                if httpResponse.statusCode == 401 {
+                    DispatchQueue.main.async {
+                        self.unauthorizedSubject.send(())
+                    }
+                    throw EasyNetError.unauthorized
+                }
+                else if (200..<300).contains(httpResponse.statusCode) {
+                    return data
+                } else if httpResponse.statusCode == 422 {
+                    throw EasyNetError.validationError(data)
+                } else if httpResponse.statusCode == 400 {
+                    throw EasyNetError.bodyPerseError
+                } else {
+                    throw EasyNetError.unknown
+                }
+                
+            }
+            .decode(type: responseType.self, decoder: decoder)
+            .retry(0)
+            .receive(on: DispatchQueue.main)
+            .subscribe(on: DispatchQueue.global())
+            .eraseToAnyPublisher()
+        return publisher
+    }
+    
+    
+    private func createRequest(withEndPoint endPoint: String, httpMethod: EasyNetMethod) -> URLRequest? {
+        guard let url = URL(string: baseUrl + endPoint) else {
+            return nil
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = httpMethod.rawValue
+        return request
     }
 }
